c1814cab3703a19e15f226ab1e5e5b9a490e6e5a
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-22 13:12:02 +0000

    watson finished

--- watson.py
+++ watson.py
@@ -1,12 +1,14 @@
 import json
 from watson_developer_cloud import ToneAnalyzerV3
+import os
+api_key = os.environ.get('WATSON_TONE_ANALYZER')
 
 class ToneAnalyzer:
 
     def __init__(self, tone_analyzer = ToneAnalyzerV3):
         self.tone_analyzer = tone_analyzer(
             version='2017-09-21',
-            iam_apikey='BdDsmJd471Ez1TtMrk3bFoQawd0ETZGwsGNo3aS7ZPCC',
+            iam_apikey=api_key,
             url= 'https://gateway-lon.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21&sentences=false'
         )
 
