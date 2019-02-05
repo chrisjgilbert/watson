6d01988e0e438a19eba347e33037a60a81c9fb38
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-05 11:57:37 +0000

    moved dep injection into init

--- response_formatter.py
+++
@@ -1,5 +0,0 @@
-# class ResponseFormatter:
-# 
-#     def format_response(self, response):
-#         for tone in response['document_tone']['tones']:
-#           print(tone['tone_name'], tone['score'])
